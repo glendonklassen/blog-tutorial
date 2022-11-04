9973328a1851aadb9d731b7d6f00dcafb2f1edf8
glendonklassen <gjklassen@protonmail.com>
2022-11-04 17:59:57 -0500

    Updated toast to use useContext

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,15 @@
 import Loader from '../components/Loader'
 import toast  from 'react-hot-toast'
+import { useContext } from "react";
+import { UserContext } from "../lib/context";
+
 
 export default function Home() {
+  const { user, username } = useContext(UserContext);
   return (
     <div>
       <h1>Homepage</h1>
-      <button onClick={() => toast.success('this is my toast!')}>Create Toast</button>
+      <button onClick={() => toast.success(`${username} is logged in`)}>Create Toast</button>
       <Loader show></Loader>
     </div>
   )
