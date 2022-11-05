99cd06672051b7dbe75300c331d6bf99b3f90e12
glendonklassen <gjklassen@protonmail.com>
2022-11-04 22:43:04 -0500

    Fixed navbar

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,44 +1,54 @@
-import Link from "next/link";
-import { useContext } from "react";
+import Link from 'next/link';
+import { useRouter } from 'next/router';
+import { useContext } from 'react';
 import { UserContext } from "../lib/context";
+import { auth } from "../lib/firebase"
 
 export default function Navbar() {
-    const { user, username } = useContext(UserContext);
+  const { user, username } = useContext(UserContext);
 
-    return (
-        <nav className="navBar">
-            <ul>
-                <li>
-                    <Link href="/">
-                        <button>Feed</button>
-                    </Link>
-                </li>
+  const router = useRouter();
 
-                { }
-                {username && (
-                    <>
-                        <li className="push-left">
-                            <Link href="/admin">
-                                <button className="btn-blue">Write Posts</button>
-                            </Link>
-                        </li>
-                        <li>
-                            <Link href={`/${username}`}>
-                                <img src={user?.photoUrl} />
-                            </Link>
-                        </li>
-                    </>
-                )}
+  const signOut = () => {
+    auth.signOut();
+    router.reload();
+  }
 
-                { }
-                {!username && (
-                    <li>
-                        <Link href="enter">
-                            <button className="btn-blue">Log in</button>
-                        </Link>
-                    </li>
-                )}
-            </ul>
-        </nav>
-    )
+  return (
+    <nav className="navbar">
+      <ul>
+        <li>
+          <Link href="/">
+            <button className="btn-logo">NXT</button>
+          </Link>
+        </li>
+
+        {username && (
+          <>
+            <li className="push-left">
+              <button onClick={signOut}>Sign Out</button>
+            </li>
+            <li>
+              <Link href="/admin">
+                <button className="btn-blue">Write Posts</button>
+              </Link>
+            </li>
+            <li>
+              <Link href={`/${username}`}>
+                <img src={user?.photoURL || '/hacker.png'} />
+              </Link>
+            </li>
+          </>
+        )}
+
+        {!username && (
+          <li>
+            <Link href="/enter">
+              <button className="btn-blue">Log in</button>
+            </Link>
+          </li>
+        )}
+      </ul>
+    </nav>
+  );
 }
