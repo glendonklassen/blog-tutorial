c83fda82a327913e358b4f11b5b5dc6bcad402f0
glendonklassen <gjklassen@protonmail.com>
2022-11-05 00:48:28 -0500

    Intermediate changes  https://fireship.io/courses/react-next-firebase/ssr-profile-page/?autoplay=true

--- pages/[username]/index.js
+++ pages/[username]/index.js
@@ -1,7 +1,22 @@
-export default function UsernameIndexPage({ }) {
+import UserProfile from "../../components/UserProfile"
+import PostFeed from "../../components/PostFeed"
+
+export async function getServerSideProps({ query }) {
+    const { username } = query;
+
+    const userDoc = await getUserWithUsername(username);
+
+    return {
+        props: { user, posts },
+    };
+}
+
+export default function UsernameIndexPage({ user, posts }) {
     return (
         <main>
             <h1>I'm a username page</h1>
+            <UserProfile user={user} />
+            <PostFeed posts={posts} />
         </main>
     )
 }
